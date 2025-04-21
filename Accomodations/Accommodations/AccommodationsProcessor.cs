169f5160e101462e83e8a2989caf1daf4110cea2
Podygun <iamonlycode@gmail.com>
2025-04-22 00:48:02 +0300

    feat: separate switch statement to methods
    also: datetime validations, guid checks

--- Accomodations/Accommodations/AccommodationsProcessor.cs
+++ Accomodations/Accommodations/AccommodationsProcessor.cs
@@ -5,7 +5,7 @@ namespace Accommodations;
 
 public static class AccommodationsProcessor
 {
-    // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ-пїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
+    // Добавлена константа для максимального кол-ва дней для брони
     private const int BOOK_MAX_DAYS = 30;
     private static BookingService _bookingService = new();
     private static Dictionary<int, ICommand> _executedCommands = new();
@@ -44,77 +44,165 @@ public static class AccommodationsProcessor
         switch ( commandName )
         {
             case "book":
-                if ( parts.Length != 6 )
-                {
-                    Console.WriteLine( "Invalid number of arguments for booking." );
-                    return;
-                }
-
-                CurrencyDto currency = ( CurrencyDto )Enum.Parse( typeof( CurrencyDto ), parts[ 5 ], true );
-
-                BookingDto bookingDto = new()
-                {
-                    UserId = int.Parse( parts[ 1 ] ),
-                    Category = parts[ 2 ],
-                    StartDate = DateTime.Parse( parts[ 3 ] ),
-                    EndDate = DateTime.Parse( parts[ 4 ] ),
-                    Currency = currency,
-                };
-
-                BookCommand bookCommand = new( _bookingService, bookingDto );
-                bookCommand.Execute();
-                _executedCommands.Add( ++s_commandIndex, bookCommand );
-                Console.WriteLine( "Booking command run is successful." );
+                HandleBook( parts );
                 break;
 
             case "cancel":
-                if ( parts.Length != 2 )
-                {
-                    Console.WriteLine( "Invalid number of arguments for canceling." );
-                    return;
-                }
-
-                Guid bookingId = Guid.Parse( parts[ 1 ] );
-                CancelBookingCommand cancelCommand = new( _bookingService, bookingId );
-                cancelCommand.Execute();
-                _executedCommands.Add( ++s_commandIndex, cancelCommand );
-                Console.WriteLine( "Cancellation command run is successful." );
+                HandleCancel( parts );
                 break;
 
             case "undo":
-                _executedCommands[ s_commandIndex ].Undo();
-                _executedCommands.Remove( s_commandIndex );
-                s_commandIndex--;
-                Console.WriteLine( "Last command undone." );
-
+                HandleUndo( parts );
                 break;
+
             case "find":
-                if ( parts.Length != 2 )
-                {
-                    Console.WriteLine( "Invalid arguments for 'find'. Expected format: 'find <BookingId>'" );
-                    return;
-                }
-                Guid id = Guid.Parse( parts[ 1 ] );
-                FindBookingByIdCommand findCommand = new( _bookingService, id );
-                findCommand.Execute();
+                HandleFind( parts );
                 break;
 
             case "search":
-                if ( parts.Length != 4 )
-                {
-                    Console.WriteLine( "Invalid arguments for 'search'. Expected format: 'search <StartDate> <EndDate> <CategoryName>'" );
-                    return;
-                }
-                DateTime startDate = DateTime.Parse( parts[ 1 ] );
-                DateTime endDate = DateTime.Parse( parts[ 2 ] );
-                string categoryName = parts[ 3 ];
-                SearchBookingsCommand searchCommand = new( _bookingService, startDate, endDate, categoryName );
-                searchCommand.Execute();
+                HandleSearch( parts );
                 break;
 
             default:
                 Console.WriteLine( "Unknown command." );
                 break;
         }
+    }
+
+    private static void HandleBook( string[] parts )
+    {
+        if ( parts.Length != 6 )
+        {
+            Console.WriteLine( "Invalid number of arguments for booking." );
+            return;
+        }
+
+        // Добавлена валидация id пользователя
+        if ( !int.TryParse( parts[ 1 ], out int userId ) )
+        {
+            Console.WriteLine( "Invalid user id." );
+            return;
+        }
+
+        // Добавлена проверка на валидность введенных пользователем дат
+        var StartDateTimeParsed = ParseDateTime( parts[ 3 ], "Invalid start date for booking." );
+        var EndDateTimeParsed = ParseDateTime( parts[ 4 ], "Invalid end date for booking." );
+
+        // Добавлена проверка оформления брони на слишком долгое время
+        TimeSpan difference = EndDateTimeParsed - StartDateTimeParsed;
+        int daysDifference = difference.Days;
+
+        if ( daysDifference > BOOK_MAX_DAYS )
+        {
+            Console.WriteLine( $"Error: Booking duration limit reached. Your selected period exceeds our maximum of {BOOK_MAX_DAYS} days." );
+            return;
+        }
+
+        // Изменен парсинг валюты, сделан вывод валидной валюты для использования
+        if ( !Enum.TryParse( parts[ 5 ], true, out CurrencyDto currency ) )
+        {
+            string validCurrency = string.Join( ", ", Enum.GetNames( typeof( CurrencyDto ) ) );
+            Console.WriteLine( $"Invalid currency: '{parts[ 5 ]}'. Use available values: {validCurrency.ToLower()}" );
+            return;
+        }
+
+        BookingDto bookingDto = new()
+        {
+            UserId = userId,
+            Category = parts[ 2 ],
+            StartDate = StartDateTimeParsed,
+            EndDate = EndDateTimeParsed,
+            Currency = currency,
+        };
+
+        BookCommand bookCommand = new( _bookingService, bookingDto );
+        bookCommand.Execute();
+        _executedCommands.Add( ++s_commandIndex, bookCommand );
+        Console.WriteLine( "Booking command run is successful." );
+    }
+
+    private static void HandleCancel( string[] parts )
+    {
+        if ( parts.Length != 2 )
+        {
+            Console.WriteLine( "Invalid number of arguments for canceling." );
+            return;
+        }
+
+        // Изменен парсинг Guid через TryParse
+        if ( !Guid.TryParse( parts[ 1 ], out Guid bookingId ) )
+        {
+            Console.WriteLine( "Invalid id for booking." );
+            return;
+        }
+
+        CancelBookingCommand cancelCommand = new( _bookingService, bookingId );
+        cancelCommand.Execute();
+        _executedCommands.Add( ++s_commandIndex, cancelCommand );
+        Console.WriteLine( "Cancellation command run is successful." );
+    }
+
+    private static void HandleUndo( string[] parts )
+    {
+        // Добавлена проверка индекса команды при отмене
+        if ( s_commandIndex < 1 )
+        {
+            Console.WriteLine( "There's no nothing to undo" );
+            return;
+        }
+        _executedCommands[ s_commandIndex ].Undo();
+        _executedCommands.Remove( s_commandIndex );
+        s_commandIndex--;
+        Console.WriteLine( "Last command undone." );
+    }
+
+    private static void HandleFind( string[] parts )
+    {
+        if ( parts.Length != 2 )
+        {
+            Console.WriteLine( "Invalid arguments for 'find'. Expected format: 'find <BookingId>'" );
+            return;
+        }
+
+        // Изменен парсинг Guid через TryParse
+        if ( !Guid.TryParse( parts[ 1 ], out Guid id ) )
+        {
+            Console.WriteLine( "Invalid id for booking." );
+            return;
+        }
+        FindBookingByIdCommand findCommand = new( _bookingService, id );
+        findCommand.Execute();
+
+        // Добавлено добавление операции в выполненные команды
+        _executedCommands.Add( ++s_commandIndex, findCommand );
+    }
+
+    private static void HandleSearch( string[] parts )
+    {
+        if ( parts.Length != 4 )
+        {
+            Console.WriteLine( "Invalid arguments for 'search'. Expected format: 'search <StartDate> <EndDate> <CategoryName>'" );
+            return;
+        }
+
+        // Добавлена проверка на валидность введенных пользователем дат
+        var startDate = ParseDateTime( parts[ 1 ], "Invalid start date for searching." );
+        var endDate = ParseDateTime( parts[ 2 ], "Invalid start date for searching." );
+
+        string categoryName = parts[ 3 ];
+        SearchBookingsCommand searchCommand = new( _bookingService, startDate, endDate, categoryName );
+        searchCommand.Execute();
+
+        // Добавлено добавление операции в выполненные команды
+        _executedCommands.Add( ++s_commandIndex, searchCommand );
+    }
+
+    private static DateTime ParseDateTime( string value, string errorMessage )
+    {
+        if ( !DateTime.TryParse( value, out DateTime dateTimeParsed ) )
+        {
+            throw new InvalidDataException( errorMessage );
+        }
+        return dateTimeParsed;
     }
 }
