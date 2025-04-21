b0805a5a6078acf295fb5f5dab87b14ac1263724
Podygun <iamonlycode@gmail.com>
2025-04-22 00:30:57 +0300

    fix: console output
    room category name and Undo message

--- Accomodations/Accommodations/Commands/FindBookingByIdCommand.cs
+++ Accomodations/Accommodations/Commands/FindBookingByIdCommand.cs
@@ -2,18 +2,20 @@ using Accommodations.Models;
 
 namespace Accommodations.Commands;
 
-public class FindBookingByIdCommand(IBookingService bookingService, Guid bookingId) : ICommand
+public class FindBookingByIdCommand( IBookingService bookingService, Guid bookingId ) : ICommand
 {
     public void Execute()
     {
-        Booking? booking = bookingService.FindBookingById(bookingId);
-        Console.WriteLine(booking != null
-            ? $"Booking found: {booking.RoomCategory} for User {booking.UserId}"
-            : "Booking not found.");
+        // Исправлен вывод названия категории
+        Booking? booking = bookingService.FindBookingById( bookingId );
+        Console.WriteLine( booking != null
+            ? $"Booking found: {booking.RoomCategory.Name} for User {booking.UserId}"
+            : "Booking not found." );
     }
 
     public void Undo()
     {
-        Console.WriteLine($"Undo operation is not supported for {nameof(GetType)}.");
+        // Изменен вывод сообщения
+        Console.WriteLine( "Undo operation is not supported for Find." );
     }
 }
