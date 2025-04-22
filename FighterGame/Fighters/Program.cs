ab0615b253a6ae25e5dfe33a0bfd8871b6450ab3
Podygun <iamonlycode@gmail.com>
2025-04-22 12:17:27 +0300

    build: cloned repository

---
+++ FighterGame/Fighters/Program.cs
@@ -0,0 +1,17 @@
+﻿using TLPractice.Models.Races;
+
+namespace Fighters
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var gameManager = new GameManager();
+
+            var winner = gameManager.Play(
+                new Knight("Tom", new Human()),
+                new Knight("Bob", new Human()));
+            Console.WriteLine($"Winner: {winner.Name}");
+        }
+    }
+}
