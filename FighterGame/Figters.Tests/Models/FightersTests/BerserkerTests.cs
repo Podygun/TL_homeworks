f51036fdb7006ed891a8d9ad7d05dc75147157a4
Podygun <iamonlycode@gmail.com>
2025-05-01 17:37:06 +0300

    feat: complete tests for Fighters

---
+++ FighterGame/Figters.Tests/Models/FightersTests/BerserkerTests.cs
@@ -0,0 +1,55 @@
+﻿using Fighters.Models.Fighters;
+
+namespace Figters.Tests.Models.FightersTests;
+
+public sealed class BerserkerTests : BaseMockFighter
+{
+    private readonly Berserker _berserker;
+
+    public BerserkerTests()
+    {
+        _berserker = new Berserker( "Test Berserker", _mockRace.Object, _mockWeapon.Object, _mockArmor.Object );
+    }
+
+
+    [Fact]
+    public void CalculateDamage_ShouldReturnCorrectDamage()
+    {
+        // Act
+        int damage = _berserker.CalculateDamage();
+
+        // Assert
+        Assert.Equal( 25, damage ); // 20 (оружие) + 5 (раса)
+    }
+
+
+    [Fact]
+    public void TakeDamage_ShouldReduceHealthCorrectly()
+    {
+        // Arrange
+        int damageToAttack = 30;
+        double knightReduction = damageToAttack * 0.2; // Блокировка части урона Berserker
+
+        _berserker.TakeDamage( damageToAttack );
+
+        // Act
+        int actualHealth = _berserker.GetCurrentHealth();
+        int expectedHealth = ( int )( 100 - ( damageToAttack - knightReduction - _berserker.CalculateArmor() ) );
+
+
+        // Assert
+        Assert.Equal( expectedHealth, actualHealth );
+    }
+
+
+    [Fact]
+    public void CalculateArmor_ShouldReturnCorrectArmor()
+    {
+        // Act
+        int currentArmor = _berserker.CalculateArmor();
+
+        // Assert
+        Assert.Equal( 15, currentArmor ); // 10 (броня расы) + 5 (броня брони)
+    }
+
+}
---
+++ FighterGame/Figters.Tests/Models/FightersTests/MageTests.cs
@@ -0,0 +1,53 @@
+﻿using Fighters.Models.Fighters;
+
+namespace Figters.Tests.Models.FightersTests;
+
+public sealed class MageTests : BaseMockFighter
+{
+    private readonly Mage _mage;
+
+    public MageTests()
+    {
+        _mage = new Mage( "Test Berserker", _mockRace.Object, _mockWeapon.Object, _mockArmor.Object );
+    }
+
+
+    [Fact]
+    public void CalculateDamage_ShouldReturnCorrectDamage()
+    {
+        // Act
+        int damage = _mage.CalculateDamage();
+
+        // Assert
+        Assert.Equal( 25, damage ); // 20 (оружие) + 5 (раса)
+    }
+
+
+    [Fact]
+    public void TakeDamage_ShouldReduceHealthCorrectly()
+    {
+        // Arrange
+        int damageToAttack = 30;
+
+        _mage.TakeDamage( damageToAttack );
+
+        // Act
+        int actualHealth = _mage.GetCurrentHealth();
+        int expectedHealth = 100 - ( damageToAttack - _mage.CalculateArmor() );
+
+        // Assert
+        Assert.Equal( expectedHealth, actualHealth );
+    }
+
+
+    [Fact]
+    public void CalculateArmor_ShouldReturnCorrectArmor()
+    {
+        // Act
+        int currentArmor = _mage.CalculateArmor();
+
+        // Assert
+        Assert.Equal( 15, currentArmor ); // 10 (броня расы) + 5 (броня брони)
+    }
+
+}
