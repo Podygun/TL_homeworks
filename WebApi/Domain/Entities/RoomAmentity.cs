0222299932303b4fe3d4a3ce55519e175858eac6
Podygun <iamonlycode@gmail.com>
2025-05-03 20:22:04 +0300

    feat (domain): add ctors to RoomAmentity and RoomService

--- WebApi/Domain/Entities/RoomAmentity.cs
+++ WebApi/Domain/Entities/RoomAmentity.cs
@@ -4,4 +4,16 @@ public sealed class RoomAmentity
 {
     public Guid Id { get; set; }
     public string Name { get; set; } = null!;
+
+    public RoomAmentity( Guid id, string name )
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public RoomAmentity( string name )
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+    }
 }
--- WebApi/Domain/Entities/RoomService.cs
+++ WebApi/Domain/Entities/RoomService.cs
@@ -4,4 +4,22 @@ public sealed class RoomService
 {
     public Guid Id { get; set; }
     public string Name { get; set; } = null!;
+
+    public RoomService( Guid id, string name )
+    {
+        Id = id;
+
+        ArgumentNullException.ThrowIfNullOrWhiteSpace( "Paramter name can't be empty" );
+
+        Name = name;
+    }
+
+    public RoomService( string name )
+    {
+        Id = Guid.NewGuid();
+
+        ArgumentNullException.ThrowIfNullOrWhiteSpace( name );
+
+        Name = name;
+    }
 }
