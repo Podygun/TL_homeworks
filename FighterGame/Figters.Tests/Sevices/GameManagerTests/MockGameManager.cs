bfb2c42b08cd149e0072675bc9b1f6ef27d6e9f3
Podygun <iamonlycode@gmail.com>
2025-05-03 11:55:15 +0300

    feat: add MockGameManager for tests

---
+++ FighterGame/Figters.Tests/Sevices/GameManagerTests/MockGameManager.cs
@@ -0,0 +1,18 @@
+﻿using Fighters;
+using Fighters.Models.Fighters;
+
+namespace Figters.Tests.Sevices.GameManagerTests;
+
+public sealed class MockGameManager : GameManager
+{
+    public static string InputString( string message )
+    {
+        return GameManager.InputString( message );
+    }
+
+    public static int InputInt( string message, int min, int max )
+    {
+        return GameManager.InputInt( message, min, max );
+    }
+
+}
