3cab1dab69440cb9533201b1ab82b0d1b13be629
Podygun <iamonlycode@gmail.com>
2025-05-03 11:56:01 +0300

    feat: add GameManagerTests class
    first init, without functional

--- FighterGame/Figters.Tests/Sevices/GameManagerTests/GameManagerTests.cs
+++ FighterGame/Figters.Tests/Sevices/GameManagerTests/GameManagerTests.cs
@@ -1,6 +1,32 @@
-﻿namespace Figters.Tests.Sevices.GameManagerTests;
+﻿using Fighters;
 
-public class GameManagerTests
+namespace Figters.Tests.Sevices.GameManagerTests;
+
+public class GameManagerTests : IDisposable
 {
+    private GameManager _gameManager;
+    private StringWriter _stringWriter;
+    private StringReader _stringReader;
+
+    public GameManagerTests()
+    {
+        _gameManager = new GameManager();
+        _stringWriter = new StringWriter();
+        Console.SetOut( _stringWriter );
+    }
+
+    public void Dispose()
+    {
+        _stringWriter.Dispose();
+        Console.SetOut( Console.Out );
+    }
+
+
+    [Fact]
+    public void CreateFighter_ShouldReturnCorrectFighter()
+    {
+
+    }
+
 
 }
