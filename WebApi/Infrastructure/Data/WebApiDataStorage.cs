79c1b14c24cab72f6e4eb4a3cb09ff11d6b4ea15
Podygun <iamonlycode@gmail.com>
2025-05-03 20:23:16 +0300

    refactor (domain): add pre-init Property entity to List

--- WebApi/Infrastructure/Data/WebApiDataStorage.cs
+++ WebApi/Infrastructure/Data/WebApiDataStorage.cs
@@ -2,9 +2,14 @@
 
 namespace Infrastructure.Data;
 
+/// <summary>
+/// Данный класс представляет коллекцию данных в виде списков, как база данных.
+/// </summary>
 internal sealed class WebApiDataStorage
 {
-    internal static List<Property> Properties = [];
+    //internal static List<Property> Properties = [ ];
+
+    internal static List<Property> Properties = [ new Property( "Test property", "Address value", "Some city", "Some country", 100d, 200d ) ];
 
     internal static List<RoomAmentity> RoomAmentities = [];
 
