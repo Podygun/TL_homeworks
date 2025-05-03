98a98724e148167665b5440b7ae1448e73e5d1ab
Podygun <iamonlycode@gmail.com>
2025-05-03 14:27:15 +0300

    feat(infrastructure): add storage for all data lists
    pseudo database

---
+++ WebApi/Infrastructure/Data/WebApiDataStorage.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+
+namespace Infrastructure.Data;
+
+internal sealed class WebApiDataStorage
+{
+    internal static List<Property> Properties = [];
+
+    internal static List<RoomAmentity> RoomAmentities = [];
+
+    internal static List<RoomService> RoomServices = [];
+
+    internal static List<RoomType> RoomTypes = [];
+}
