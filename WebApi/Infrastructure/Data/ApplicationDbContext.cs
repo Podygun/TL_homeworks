e2b2f81390d7c7e3864e74830a033712f1f74a9d
Podygun <iamonlycode@gmail.com>
2025-05-10 18:50:03 +0300

    feat(infrastructure): add ApplicationDbcontext
    for work with sqlite

---
+++ WebApi/Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public sealed class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext( DbContextOptions<ApplicationDbContext> options )
+        : base( options )
+    {
+    }
+
+    public DbSet<Property> Properties { get; set; }
+    public DbSet<RoomAmentity> RoomAmentities { get; set; }
+    public DbSet<RoomService> RoomServices { get; set; }
+    public DbSet<RoomType> RoomTypes { get; set; }
+
+    protected override void OnModelCreating( ModelBuilder modelBuilder )
+    {
+        // Связь один-ко-многим между Property и RoomType
+        modelBuilder.Entity<RoomType>()
+            .HasOne( rt => rt.Property )
+            .WithMany( p => p.RoomTypes )
+            .HasForeignKey( rt => rt.PropertyId );
+
+        // Связь многие-ко-многим между RoomType и RoomService
+        modelBuilder.Entity<RoomType>()
+            .HasMany( rt => rt.RoomServices )
+            .WithMany( rs => rs.RoomTypes )
+            .UsingEntity( j => j.ToTable( "RoomTypeRoomServices" ) );
+
+        // Связь многие-ко-многим между RoomType и RoomAmentity
+        modelBuilder.Entity<RoomType>()
+            .HasMany( rt => rt.RoomAmentities )
+            .WithMany( ra => ra.RoomTypes )
+            .UsingEntity( j => j.ToTable( "RoomTypeRoomAmentities" ) );
+    }
+}
