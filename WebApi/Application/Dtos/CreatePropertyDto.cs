d1cad71712832f55acbc33ced3e1d40f6d7d132c
Podygun <iamonlycode@gmail.com>
2025-05-10 23:32:13 +0300

    feat (application): add Application layer

---
+++ WebApi/Application/Dtos/CreatePropertyDto.cs
@@ -0,0 +1,22 @@
+﻿namespace Application.Dtos;
+
+public class CreatePropertyDto
+{
+    public string Name { get; set; } = null!;
+    public string Country { get; set; } = null!;
+    public string City { get; set; } = null!;
+    public string Address { get; set; } = null!;
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+
+    public CreatePropertyDto( string name, string country,
+        string city, string address, double latitude, double longitude )
+    {
+        Name = name;
+        Country = country;
+        City = city;
+        Address = address;
+        Latitude = latitude;
+        Longitude = longitude;
+    }
+}
