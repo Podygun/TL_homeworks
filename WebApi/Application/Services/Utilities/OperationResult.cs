222821557f5433319ea1c738cb63f465053918ba
Podygun <iamonlycode@gmail.com>
2025-05-19 15:20:34 +0300

    refactor (application): update OperationResult to enum

--- WebApi/Application/Services/Utilities/OperationResult.cs
+++ WebApi/Application/Services/Utilities/OperationResult.cs
@@ -1,21 +1,9 @@
 ﻿namespace Application.Services.Utilities;
 
-public sealed class OperationResult
+public enum OperationResult
 {
-    public bool Success { get; set; }
-    public string? ErrorMessage { get; set; }
-
-    public OperationResult( bool success, string? errorMessage = null )
-    {
-        Success = success;
-        ErrorMessage = errorMessage;
-    }
-
-    /// <summary>
-    /// Default value of Success is True
-    /// </summary>
-    public OperationResult()
-    {
-        Success = true;
-    }
+    Ok = 0,
+    Success = 1,
+    BadRequest = 2,
+    ServerError = 3
 }
