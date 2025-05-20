b61f4e3bb496d3d696d14a956c66d844e80e3c34
Podygun <iamonlycode@gmail.com>
2025-05-20 23:08:23 +0300

    refactor: remove unnecessary EventListener
    remove DOMContentLoaded, coz have type=module

--- JsonDiff/index.js
+++ JsonDiff/index.js
@@ -2,9 +2,6 @@ import { init } from "./auth.js";
 import { jsonDiffForm } from "./form-handler.js";
 import { formValidation } from './form-validator.js';
 
-
-document.addEventListener('DOMContentLoaded', () => {
-  init();  
-  jsonDiffForm();
-  formValidation();
-});
+init();  
+jsonDiffForm();
+formValidation();
