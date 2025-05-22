faa6d32cc515e58a36f357246b0250e7caa922a3
Podygun <iamonlycode@gmail.com>
2025-05-22 17:09:43 +0300

    style: remove comments, short namespace definition

--- FighterGame/Figters.Tests/Models/FightersTests/BaseFighterTests/TestFighter.cs
+++ FighterGame/Figters.Tests/Models/FightersTests/BaseFighterTests/TestFighter.cs
@@ -1,13 +1,12 @@
 ﻿using Fighters.Models.Fighters;
 
-namespace Figters.Tests.Models.FightersTests.BaseFighterTests
-{
-    public sealed class TestFighter : FighterBase
-    {
-        public TestFighter( string name, int maxHealth ) : base( name, maxHealth ) { }
+namespace Figters.Tests.Models.FightersTests.BaseFighterTests;
 
-        public override int CalculateArmor() => 0; // Реализация абстрактного метода
-        public override int CalculateDamage() => 0; // Реализация абстрактного метода
-        public override string GetDescription() => "Тестовый боец"; // Реализация абстрактного
-    }
+public sealed class TestFighter : FighterBase
+{
+    public TestFighter( string name, int maxHealth ) : base( name, maxHealth ) { }
+
+    public override int CalculateArmor() => 0;
+    public override int CalculateDamage() => 0;
+    public override string GetDescription() => "Тестовый боец";
 }
