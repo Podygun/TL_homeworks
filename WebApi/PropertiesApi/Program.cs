b0c158aeeb7af672d242e6b58754bd22d40dda84
Podygun <iamonlycode@gmail.com>
2025-05-23 14:50:32 +0300

    style: program.cs

--- WebApi/PropertiesApi/Program.cs
+++ WebApi/PropertiesApi/Program.cs
@@ -1,63 +1,54 @@
-using Application.Mappers;
 using Application.Services.PropertiesServices;
-using Application.Services.RoomAmentitiesServices;
 using Application.Services.RoomTypesServices;
-using Domain.Entities;
 using Domain.Repositories;
 using Infrastructure.Data;
 using Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
-namespace PropertiesApi
+namespace PropertiesApi;
+
+public class Program
 {
-    public class Program
+    public static void Main( string[] args )
     {
-        public static void Main( string[] args )
+        WebApplicationBuilder builder = WebApplication.CreateBuilder( args );
+
+        RegisterServices( builder );
+
+        WebApplication app = builder.Build();
+
+        if ( app.Environment.IsDevelopment() )
         {
-            WebApplicationBuilder builder = WebApplication.CreateBuilder( args );
-
-            RegisterServices( builder );
-
-            WebApplication app = builder.Build();
-
-            if ( app.Environment.IsDevelopment() )
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
 
-        public static void RegisterServices( WebApplicationBuilder builder )
-        {
-            // SQLite database connection
-            builder.Services.AddDbContext<ApplicationDbContext>( options =>
-                options.UseSqlite( builder.Configuration.GetConnectionString( "DefaultConnection" ) ) );
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
 
-            // Repos (Infrastructure)
-            builder.Services.AddScoped<IPropertiesRepository, PropertiesRepository>();
-            builder.Services.AddScoped<IRoomServicesRepository, RoomServiceRepository>();
-            builder.Services.AddScoped<IRoomAmentitiesRepository, RoomAmentityRepository>();
-            builder.Services.AddScoped<IRoomTypesRepository, RoomTypeRepository>();
+        app.Run();
+    }
 
-            //Services (Application)
-            builder.Services.AddScoped<IPropertiesService, PropertiesService>();
-            builder.Services.AddScoped<IRoomTypesService, RoomTypesService>();
+    public static void RegisterServices( WebApplicationBuilder builder )
+    {
+        // SQLite database connection
+        builder.Services.AddDbContext<ApplicationDbContext>( options =>
+            options.UseSqlite( builder.Configuration.GetConnectionString( "DefaultConnection" ) ) );
 
+        // Repos (Infrastructure)
+        builder.Services.AddScoped<IPropertiesRepository, PropertiesRepository>();
+        builder.Services.AddScoped<IRoomServicesRepository, RoomServicesRepository>();
+        builder.Services.AddScoped<IRoomAmentitiesRepository, RoomAmentitiesRepository>();
+        builder.Services.AddScoped<IRoomTypesRepository, RoomTypesRepository>();
 
+        //Services (Application)
+        builder.Services.AddScoped<IPropertiesService, PropertiesService>();
+        builder.Services.AddScoped<IRoomTypesService, RoomTypesService>();
 
-
-            // Built-in services
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-        }
+        // Built-in services
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
     }
 }
--- WebApi/ReservationApi/Program.cs
+++ WebApi/ReservationApi/Program.cs
@@ -1,54 +1,56 @@
-
+using Application.Services.PropertiesServices;
+using Application.Services.RoomTypesServices;
 using Domain.Repositories;
 using Infrastructure.Data;
 using Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
-namespace ReservationApi
+namespace ReservationApi;
+
+public class Program
 {
-    public class Program
+    public static void Main( string[] args )
     {
-        public static void Main( string[] args )
+        WebApplicationBuilder builder = WebApplication.CreateBuilder( args );
+
+        RegisterServices( builder );
+
+        WebApplication app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if ( app.Environment.IsDevelopment() )
         {
-            WebApplicationBuilder builder = WebApplication.CreateBuilder( args );
-
-            RegisterServices( builder );
-
-            WebApplication app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if ( app.Environment.IsDevelopment() )
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
 
-        public static void RegisterServices( WebApplicationBuilder builder )
-        {
-            // SQLite database connection
-            builder.Services.AddDbContext<ApplicationDbContext>( options =>
-                options.UseSqlite( builder.Configuration.GetConnectionString( "DefaultConnection" ) ) );
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
 
-            // DI services
-            builder.Services.AddScoped<IPropertiesRepository, PropertiesRepository>();
-            builder.Services.AddScoped<IRoomServicesRepository, RoomServiceRepository>();
-            builder.Services.AddScoped<IRoomAmentitiesRepository, RoomAmentityRepository>();
-            builder.Services.AddScoped<IRoomTypesRepository, RoomTypeRepository>();
+        app.Run();
+    }
 
-            // Built-in services
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-        }
+    public static void RegisterServices( WebApplicationBuilder builder )
+    {
+        // SQLite database connection
+        builder.Services.AddDbContext<ApplicationDbContext>( options =>
+            options.UseSqlite( builder.Configuration.GetConnectionString( "DefaultConnection" ) ) );
+
+        // Services (Infrastructure)
+        builder.Services.AddScoped<IPropertiesRepository, PropertiesRepository>();
+        builder.Services.AddScoped<IRoomServicesRepository, RoomServicesRepository>();
+        builder.Services.AddScoped<IRoomAmentitiesRepository, RoomAmentitiesRepository>();
+        builder.Services.AddScoped<IRoomTypesRepository, RoomTypesRepository>();
+        builder.Services.AddScoped<IReservationsRepository, ReservationRepository>();
+
+        //Services (Application)
+        builder.Services.AddScoped<IPropertiesService, PropertiesService>();
+        builder.Services.AddScoped<IRoomTypesService, RoomTypesService>();
+
+        // Built-in services
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
     }
 }
