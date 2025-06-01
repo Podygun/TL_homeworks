1d9a36035240189d9618c42510f4327d4c90e6fd
Podygun <iamonlycode@gmail.com>
2025-06-02 00:59:19 +0300

    feat: add notes

---
+++ EventCalendar/event-calendar/src/components/Notes/Ghosts/DragNote.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import type { PreviewProps } from "../../types/types";
+import styles from './Previews.module.css';
+
+const DragNote: React.FC<PreviewProps> = ({ x, y, note }) => {
+  return (
+    <div
+      className={styles.dragNotePreview}
+      style={{
+        top: y,
+        left: x,
+      }}
+    >
+      <div className={styles.previewBody}>
+        <p className={styles.previewText}>{note.content}</p>
+      </div>
+    </div>
+  );
+};
+
+export default DragNote;
---
+++ EventCalendar/event-calendar/src/components/Notes/Ghosts/NewNotePreview.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from './Previews.module.css';
+
+const NewNotePreview: React.FC = () => {
+  return <div className={styles.newNotePreview} />;
+};
+
+export default NewNotePreview;
