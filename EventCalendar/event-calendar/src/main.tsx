6a0772493103cc48092a7f4e57f1d35ba83625e5
Podygun <iamonlycode@gmail.com>
2025-06-02 00:57:58 +0300

    style: clean build tsx files, add common css

--- EventCalendar/event-calendar/src/main.tsx
+++ EventCalendar/event-calendar/src/main.tsx
@@ -1,11 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
-import DraggableBoxes from './App2.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <DraggableBoxes />
+    <App />
   </StrictMode>,
 )
