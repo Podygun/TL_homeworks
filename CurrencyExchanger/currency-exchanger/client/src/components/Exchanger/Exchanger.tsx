ea732804856edf70c4b93686f0d82286657f16ec
Podygun <iamonlycode@gmail.com>
2025-06-04 23:20:16 +0300

    feat: complete loader/error/container logic

--- CurrencyExchanger/currency-exchanger/client/src/components/Exchanger/Exchanger.tsx
+++ CurrencyExchanger/currency-exchanger/client/src/components/Exchanger/Exchanger.tsx
@@ -3,6 +3,9 @@ import { fetchCurrencies, fetchLatestRate, CurrencyInfo } from '../api/currencyA
 import { formatNumber, formatUpdateTime } from '../utils/formatters';
 import styles from './Exchanger.module.css';
 import CurrencyDescription from '../CurrencyDescription/CurrencyDescription';
+import CurrencyDivider from '../СurrencyDivider/CurrencyDivider';
+import Loader from '../Loader/Loader';
+import ErrorMessage from '../Error/ErrorMessage';
 
 export default function ExchangeWidget() {
   const [currencies, setCurrencies] = useState<CurrencyInfo[]>([]);
@@ -12,17 +15,34 @@ export default function ExchangeWidget() {
   const [baseAmount, setBaseAmount] = useState<string>('1');
   const [convertedAmount, setConvertedAmount] = useState<number>(0);
   const [lastUpdateTime, setLastUpdateTime] = useState<Date | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   const baseCurrencyInfo = currencies.find((c) => c.code === baseCurrency);
   const targetCurrencyInfo = currencies.find((c) => c.code === targetCurrency);
 
+  const fromDateTimeApi = '2000-01-01T00:00:00';
+
+  const handleError = (message: string) => {
+    setErrorMessage(message);
+    setIsLoading(false);
+  };
+
   useEffect(() => {
-    fetchCurrencies().then(setCurrencies).catch(console.error);
+    setIsLoading(true);
+    fetchCurrencies()
+      .then((data) => {
+        setCurrencies(data);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
 
   useEffect(() => {
-    const fromDateTime = '2000-01-01T00:00:00';
-    fetchLatestRate(baseCurrency, targetCurrency, fromDateTime)
+    if (!baseCurrency || !targetCurrency) return;
+    setIsLoading(true);
+    fetchLatestRate(baseCurrency, targetCurrency, fromDateTimeApi)
       .then((data) => {
         if (data) {
           setRate(data.price);
@@ -32,7 +52,10 @@ export default function ExchangeWidget() {
           setLastUpdateTime(null);
         }
       })
-      .catch(console.error);
+      .catch(() => handleError('COULD NOT GET DATA FROM THE SERVER'))
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, [baseCurrency, targetCurrency]);
 
   // Расчет итоговой единицы (при изменении параметров)
@@ -46,86 +69,86 @@ export default function ExchangeWidget() {
 
     if (targetCurrency === baseCurrency) {
       setConvertedAmount(amountNum);
-      return;
-    }
-
-    if (rate) {
-      setConvertedAmount(amountNum * rate);
     } else {
-      setConvertedAmount(0);
+      setConvertedAmount(rate ? amountNum * rate : 0);
     }
   }, [baseAmount, targetCurrency, baseCurrency, rate]);
 
   const handleBaseAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const val = e.target.value;
-    // Разрешаем только числа и точку
     if (/^\d*\.?\d*$/.test(val)) {
       setBaseAmount(val);
     }
   };
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.header}>
-        <div>
-          {baseAmount || 0} {baseCurrency} is
-        </div>
-        <div>
-          {formatNumber(convertedAmount)} {targetCurrency}
-        </div>
-        <div className={styles.time}>{lastUpdateTime ? `Last update: ${formatUpdateTime(lastUpdateTime)}` : ''}</div>
-      </div>
-      <div className={styles.converterRow}>
-        <input
-          type="text"
-          inputMode="decimal"
-          value={baseAmount}
-          onChange={handleBaseAmountChange}
-          className={styles.input}
-          aria-label="Base amount"
-        />
-        <select value={baseCurrency} onChange={(e) => setBaseCurrency(e.target.value)}>
-          {currencies.map((c) => (
-            <option key={c.code} value={c.code}>
-              {c.code} - {c.name}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className={styles.converterRow}>
-        <input
-          type="text"
-          value={formatNumber(convertedAmount)}
-          readOnly
-          className={styles.input}
-          aria-label="Converted amount"
-        />
-        <select value={targetCurrency} onChange={(e) => setTargetCurrency(e.target.value)}>
-          {currencies.map((c) => (
-            <option key={c.code} value={c.code}>
-              {c.code} - {c.name}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div>
-        {rate !== null ? (
-          <>
-            <p>Rate: {rate}</p>
-          </>
-        ) : (
-          //TODO Loading icon
-          <p>Loading rate...</p>
-        )}
-      </div>
-      {baseCurrencyInfo === targetCurrencyInfo ? (
-        baseCurrencyInfo && <CurrencyDescription currency={baseCurrencyInfo} />
-      ) : (
-        <>
-          {baseCurrencyInfo && <CurrencyDescription currency={baseCurrencyInfo} />}
-          {targetCurrencyInfo && <CurrencyDescription currency={targetCurrencyInfo} />}
-        </>
-      )}
-    </div>
+  const renderCurrencyDescriptions = () => (
+    <>
+      {baseCurrencyInfo && <CurrencyDescription currency={baseCurrencyInfo} />}
+      {targetCurrencyInfo && <CurrencyDescription currency={targetCurrencyInfo} />}
+    </>
   );
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (errorMessage) {
+    return <ErrorMessage message={errorMessage} />;
+  }
+
+  if (currencies.length !== 0) {
+    return (
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <div className={styles.fromCurrencyTitle}>
+            {1} {baseCurrencyInfo?.name} is
+          </div>
+          <div className={styles.toCurrencyTitle}>
+            {1 * (rate ?? 0)} {targetCurrencyInfo?.name}
+          </div>
+          <div className={styles.time}>{lastUpdateTime ? `Last update: ${formatUpdateTime(lastUpdateTime)}` : ''}</div>
+        </div>
+
+        <div className={styles.converterRow}>
+          <input
+            type="text"
+            inputMode="decimal"
+            value={baseAmount}
+            onChange={handleBaseAmountChange}
+            className={styles.input}
+            aria-label="Base amount"
+          />
+          <select value={baseCurrency} onChange={(e) => setBaseCurrency(e.target.value)} className={styles.select}>
+            {currencies.map((c) => (
+              <option key={c.code} value={c.code}>
+                {c.code}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className={styles.converterRow}>
+          <input
+            type="text"
+            value={formatNumber(convertedAmount)}
+            readOnly
+            className={styles.input}
+            aria-label="Converted amount"
+          />
+          <select value={targetCurrency} onChange={(e) => setTargetCurrency(e.target.value)} className={styles.select}>
+            {currencies.map((c) => (
+              <option key={c.code} value={c.code}>
+                {c.code}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        {baseCurrencyInfo && targetCurrencyInfo && (
+          <CurrencyDivider baseCode={baseCurrencyInfo.code} targetCode={targetCurrencyInfo.code} />
+        )}
+
+        {renderCurrencyDescriptions()}
+      </div>
+    );
+  }
 }
