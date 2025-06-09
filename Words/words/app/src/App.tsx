9018e58e29c81af9e9f595dc43e25693c029089d
Podygun <iamonlycode@gmail.com>
2025-06-09 18:12:29 +0300

    feat: clean, style original files

--- Words/words/app/src/App.tsx
+++ Words/words/app/src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HomePage } from "./pages/HomePage/HomePage";
+import { DictionaryPage } from "./pages/DictionaryPage/DictionaryPage";
+import { NewPairPage } from "./pages/PairPages/NewPairPage";
+import { EditPairPage } from "./pages/PairPages/EditPairPage";
+import { TestingPage } from "./pages/TestingPage/TestingPage";
+import { ResultsPage } from "./pages/ResultsPage/ResultsPage";
+import "./styles/app.scss";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="dictionary" element={<DictionaryPage />} />
+          <Route path="newpair" element={<NewPairPage />} />
+          <Route path="editpair" element={<EditPairPage />} />
+          <Route path="testing" element={<TestingPage />} />
+          <Route path="results" element={<ResultsPage />} />
+        </Routes>
+      </Router>
+  );
+};
 
-export default App
+export default App;
