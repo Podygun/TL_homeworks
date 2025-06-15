33f1f504a1023f5a83600b83ed19480f42d91dbb
Podygun <iamonlycode@gmail.com>
2025-06-15 16:15:54 +0300

    feat: add click functional

--- CurrencyExchanger/currency-exchanger/client/src/components/СurrencyDivider/CurrencyDivider.tsx
+++ CurrencyExchanger/currency-exchanger/client/src/components/СurrencyDivider/CurrencyDivider.tsx
@@ -3,15 +3,26 @@ import styles from './CurrencyDivider.module.css';
 type CurrencyDividerProps = {
   baseCode: string;
   targetCode: string;
+  onClick: () => void;
+  isExpanded: boolean;
 };
 
-export default function CurrencyDivider({ baseCode, targetCode }: CurrencyDividerProps) {
+export default function CurrencyDivider({ 
+  baseCode, 
+  targetCode, 
+  onClick,
+  isExpanded 
+}: CurrencyDividerProps) {
   return (
     <div className={styles.wrapper}>
       <hr className={styles.line} />
-      <div className={styles.label}>
-        {baseCode}/{targetCode}: about ↑
-      </div>
+      <button 
+        className={styles.label} 
+        onClick={onClick}
+        aria-expanded={isExpanded}
+      >
+        {baseCode}/{targetCode}: about {isExpanded ? '↑' : '↓'}
+      </button>
     </div>
   );
 }
