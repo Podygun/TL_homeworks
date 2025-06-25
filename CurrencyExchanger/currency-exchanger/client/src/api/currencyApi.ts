802c462b7bc574110fb386b0153a87d17d8e1ea0
Podygun <iamonlycode@gmail.com>
2025-06-25 14:24:38 +0300

    refactor: update api

--- CurrencyExchanger/currency-exchanger/client/src/api/currencyApi.ts
+++ CurrencyExchanger/currency-exchanger/client/src/api/currencyApi.ts
@@ -1,6 +1,6 @@
 import { CurrencyInfo, PriceEntry } from "../components/Types";
 
-const BASE_URL = 'https://localhost:7145';
+const BASE_URL = 'http://localhost:5081';
 
 export async function fetchCurrencies(): Promise<CurrencyInfo[]> {
   const res = await fetch(`${BASE_URL}/Currency`);
@@ -14,32 +14,37 @@ export async function fetchCurrency(code: string): Promise<CurrencyInfo> {
   return res.json();
 }
 
-export async function fetchLatestRate(
-  paymentCurrency: string,
-  purchasedCurrency: string,
-  fromDateTime: string
-): Promise<{ price: number; dateTime: string } | null> {
-  const url = `${BASE_URL}/prices/?PaymentCurrency=${encodeURIComponent(
-    paymentCurrency
-  )}&PurchasedCurrency=${encodeURIComponent(purchasedCurrency)}&FromDateTime=${encodeURIComponent(fromDateTime)}`;
-  console.log(url);
-
-  const res = await fetch(url);
-  if (!res.ok) throw new Error('Failed to fetch rates');
-  const data: PriceEntry[] = await res.json();
-  if (data.length === 0) return null;
-  return data[data.length - 1];
+export function getLatestRate(rates: PriceEntry[]): { price: number; dateTime: string } | null {
+  return rates.length > 0 ? rates[rates.length - 1] : null;
 }
+
+// export async function fetchRates(
+//   paymentCurrency: string,
+//   purchasedCurrency: string,
+//   fromDateTime: string
+// ): Promise<PriceEntry[]> {
+//   const url = `${BASE_URL}/prices/?PaymentCurrency=${encodeURIComponent(
+//     paymentCurrency
+//   )}&PurchasedCurrency=${encodeURIComponent(purchasedCurrency)}&FromDateTime=${encodeURIComponent(fromDateTime)}`;
+//   const res = await fetch(url);
+//   if (!res.ok) throw new Error('Failed to fetch prices');
+//   return res.json();
+// }
+
 
 export async function fetchRates(
   paymentCurrency: string,
   purchasedCurrency: string,
   fromDateTime: string
 ): Promise<PriceEntry[]> {
+
   const url = `${BASE_URL}/prices/?PaymentCurrency=${encodeURIComponent(
     paymentCurrency
   )}&PurchasedCurrency=${encodeURIComponent(purchasedCurrency)}&FromDateTime=${encodeURIComponent(fromDateTime)}`;
+
   const res = await fetch(url);
   if (!res.ok) throw new Error('Failed to fetch prices');
-  return res.json();
+  
+  const data = await res.json();
+  return data;
 }
